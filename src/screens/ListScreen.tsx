b7333bf7824ef0650bcff388d4b116f3aa3aaea0
Furkan AKGUN <fakgun@yukselproje.com.tr>
2022-01-06 13:14:10 +0300

    ability to  add task

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import { nanoid } from "nanoid";
+import React, { ChangeEvent, KeyboardEvent, useState } from "react";
 
 type Props = {};
 
-const ListScreen: React.FC<Props> = () => {
+type Task = {
+    id:string;
+    label: string;
+}
 
+const ListScreen: React.FC<Props> = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTaskLabel,setNewTaskLabel] = useState('');
+
+    const handleNewTaskLabelChange = (e: ChangeEvent<HTMLInputElement>) => setNewTaskLabel(e.target.value);
+
+    const handleNewTaskKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key == "Enter" && newTaskLabel != ''){
+            setTasks(tasks => [...tasks,{id: nanoid(),label: newTaskLabel}])
+            setNewTaskLabel('');
+        }
+    }
     return (
-        <div> List View 2 </div>
+        <div>
+            <ul>
+                {tasks.map((task) => <li key={task.id}> {task.label} </li>)}
+            </ul>
+            <input value={newTaskLabel} onChange={handleNewTaskLabelChange} onKeyPress={handleNewTaskKeyPress}>
+            </input>
+        </div>
     )
 
 }
