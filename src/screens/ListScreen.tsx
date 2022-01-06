e13a6a8349c571ecaa4a4ef32b71fa8ea0212160
Furkan AKGUN <fakgun@yukselproje.com.tr>
2022-01-06 14:01:40 +0300

    clear tasks

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -4,29 +4,50 @@ import React, { ChangeEvent, KeyboardEvent, useState } from "react";
 type Props = {};
 
 type Task = {
-    id:string;
+    id: string;
     label: string;
+    isComplete: boolean;
 }
 
 const ListScreen: React.FC<Props> = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
-    const [newTaskLabel,setNewTaskLabel] = useState('');
+    const [newTaskLabel, setNewTaskLabel] = useState('');
 
     const handleNewTaskLabelChange = (e: ChangeEvent<HTMLInputElement>) => setNewTaskLabel(e.target.value);
 
     const handleNewTaskKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
-        if(e.key == "Enter" && newTaskLabel != ''){
-            setTasks(tasks => [...tasks,{id: nanoid(),label: newTaskLabel}])
+        if (e.key == "Enter" && newTaskLabel != '') {
+            setTasks(tasks => [...tasks, { id: nanoid(), label: newTaskLabel, isComplete: false }])
             setNewTaskLabel('');
         }
     }
+
+    const handleCompleteChange = (handledTask: Task) => (e: ChangeEvent<HTMLInputElement>) => {
+        setTasks((tasks => tasks.map((task) => {
+            if (task.id === handledTask.id) {
+                return { ...task, isComplete: e.target.checked }
+            } else {
+                return task;
+            }
+        })));
+    };
+
+    const handleClearClick = () => {
+        setTasks(tasks => tasks.filter(task => !task.isComplete));
+    }
+
+    console.log(tasks);
     return (
         <div>
-            <ul>
-                {tasks.map((task) => <li key={task.id}> {task.label} </li>)}
-            </ul>
+            <div>
+                {tasks.map((task) => <div key={task.id}> <input type="checkbox" defaultChecked={task.isComplete} onChange={handleCompleteChange(task)}></input> {task.label} </div>)}
+            </div>
             <input value={newTaskLabel} onChange={handleNewTaskLabelChange} onKeyPress={handleNewTaskKeyPress}>
             </input>
+            <div>
+                <button onClick={handleClearClick}>Clear Completed</button>
+            </div>
+
         </div>
     )
 
